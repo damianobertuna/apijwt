20a8ddd1ec06bfd962f0c776769496d20fd77240
Damiano Bertuna <damianobertuna@gmail.com>
2021-06-06 08:47:35 +0200

    Implemented some unit tests

--- src/autoload.php
+++ src/autoload.php
@@ -1,23 +1,39 @@
 <?php 
-	spl_autoload_register(function($className){
-		$srcPaths[] = './vendor/firebase/php-jwt/src/';
-		$srcPaths[] = './vendor/guzzlehttp/guzzle/src/';
-		$srcPaths[] = './vendor/guzzlehttp/guzzle/src/Cookie/';
-		$srcPaths[] = './vendor/guzzlehttp/guzzle/src/Exception/';
-		$srcPaths[] = './vendor/guzzlehttp/guzzle/src/Handler/';
-		$srcPaths[] = './class/';
-		$srcPaths[] = './config/';
+require_once('./config/config.php');
+require_once('./config/globals.php');
 
-		foreach ($srcPaths as $srcPath) {
-			$srcPathFiles = scandir($srcPath);
-			foreach ($srcPathFiles as $subdirClass) {
-				// per src di guzzle bisogna far saltare le cartelle
-				// quindi subdirClass deve contenere .php
-				if ($subdirClass != "." && $subdirClass != "..") {
+spl_autoload_register(function($className){
+	$srcPaths[] = './vendor/firebase/php-jwt/src/';
+	$srcPaths[] = './vendor/guzzlehttp/guzzle/src/';
+	$srcPaths[] = './vendor/psr/http-client/src/';
+	/*$srcPaths[] = './vendor/guzzlehttp/guzzle/src/Cookie/';
+	$srcPaths[] = './vendor/guzzlehttp/guzzle/src/Exception/';
+	$srcPaths[] = './vendor/guzzlehttp/guzzle/src/Handler/';*/
+	$srcPaths[] = './class/';
+
+	foreach ($srcPaths as $srcPath) {
+		$classNameTmp = explode("\\", $className);
+		if (count($classNameTmp) > 0) {
+			$className = $classNameTmp[count($classNameTmp)-1];
+		}
+		
+		if (file_exists($srcPath.$className.'.php')) {
+			require_once($srcPath.$className.'.php');
+			return;
+		}			
+		/*$srcPathFiles = scandir($srcPath);
+		foreach ($srcPathFiles as $subdirClass) {
+			// per src di guzzle bisogna far saltare le cartelle
+			// quindi subdirClass deve contenere .php
+			if ($subdirClass != "." && $subdirClass != "..") {
+				if (strpos($subdirClass, ".php")) {
 					require_once($srcPath.$subdirClass);
+				} else {
+					$srcPaths[] = $srcPath.$subdirClass."/";
 				}
 			}
-		}
-	})
+		}*/
+	}
+})
 
- ?>
+?>
--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,27 +1,116 @@
 <?php 
 declare(strict_types=1);
 
+require_once('src/autoload.php');
+
+use GuzzleHttp\Exception\ServerException;
 use PHPUnit\Framework\TestCase;
 
 final class ApiTest extends TestCase
 {
     public function test_username_is_requred(): void
-    {
+    {        
         $loginJson = array(
             "name"  => "actionLogin",
             "param" => array(
                 "username"     => "",
-                "password"  => "12345"
+                "password"  => "newpassword"
             )
         );
 
-        $responseObj = new Response;
+        $client = new GuzzleHttp\Client();
+        try {      
+            $client->request('POST', 'localhost/apijwt/index.php', [
+                'json' => $loginJson
+            ]);
+        } catch (ServerException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString   = $response->getBody()->getContents();
+            $responseBodyAsObj      = json_decode($responseBodyAsString);            
+            $this->assertEquals(500, $responseBodyAsObj->response->status);
+            $this->assertEquals("Invalid user or password", $responseBodyAsObj->response->message);
+        }
+    }
 
-        $api = new Api($responseObj);
-        $api->request = json_encode($loginJson);
-        $api->processApi();
-        //$response = json_decode($response);
-        $this->assertEquals(400, $responseObj->getStatus());
+    public function test_password_is_requred(): void
+    {        
+        $loginJson = array(
+            "name"  => "actionLogin",
+            "param" => array(
+                "username"     => "test@email.com",
+                "password"  => ""
+            )
+        );
+
+        $client = new GuzzleHttp\Client();
+        try {      
+            $client->request('POST', 'localhost/apijwt/index.php', [
+                'json' => $loginJson
+            ]);
+        } catch (ServerException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString   = $response->getBody()->getContents();
+            $responseBodyAsObj      = json_decode($responseBodyAsString);            
+            $this->assertEquals(500, $responseBodyAsObj->response->status);
+            $this->assertEquals("Invalid user or password", $responseBodyAsObj->response->message);
+        }
+    }
+
+    public function test_get_resource_only_logged_in_user(): void
+    {   
+        $loginJson = array(
+            "name"  => "actionGetResource",
+            "param" => array(
+                "id"     => "123",                
+            )
+        );
+
+        $client = new GuzzleHttp\Client();
+        try {      
+            $client->request('POST', 'localhost/apijwt/index.php', [
+                'json' => $loginJson
+            ]);
+        } catch (ServerException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString   = $response->getBody()->getContents();
+            $responseBodyAsObj      = json_decode($responseBodyAsString);
+            //var_dump($responseBodyAsObj);
+            $this->assertEquals(500, $responseBodyAsObj->response->status);
+            $this->assertEquals("Access Token Not found", $responseBodyAsObj->response->message);
+        }
+    }
+
+    public function test_change_password_new_not_empty(): void
+    {   
+        $loginJson = array(
+            "name"  => "actionChangePassword",
+            "param" => array(
+                "username"        => "test@email.com",          
+                "oldPassword"     => "mypassword",
+                "newPassword"     => "",
+            )
+        );
+
+        $client = new GuzzleHttp\Client();
+        $jar = \GuzzleHttp\Cookie\CookieJar::fromArray(
+            [
+                'JwtRefreshToken' => 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjI5NjAyMzAsImlzcyI6ImxvY2FsaG9zdCIsImV4cCI6MTYyMzU2NTAzMH0.wTbwVq8dg6718-E4f9FFv5SS-xwq6YfhSXf7rWtazUw',
+            ],
+            'localhost'
+        );
+        try {      
+            $res = $client->request('POST', 'localhost/apijwt/index.php', [
+                'json' => $loginJson,
+                'cookies' => $jar
+            ]);
+        } catch (ServerException $e) {
+            $response = $e->getResponse();
+            $responseBodyAsString   = $response->getBody()->getContents();
+            $responseBodyAsObj      = json_decode($responseBodyAsString);
+            var_dump($responseBodyAsObj);
+            $this->assertEquals(500, $responseBodyAsObj->response->status);
+            $this->assertEquals("Access Token Not found", $responseBodyAsObj->response->message);
+        }
     }
 
 }
